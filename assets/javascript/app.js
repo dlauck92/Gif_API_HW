d568d9485205f4c32c0f7f5b0464a2aad414d4fa
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-12-06 18:11:30 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
     // display gifs
     function displayGifs() {
         var animal = $(this).attr("data-animal");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
         console.log(queryURL);
 
         $.ajax({
